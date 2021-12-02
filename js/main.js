193e309ce39ee9c601ff0a3a47cc18e0f9ca0eb3
vstro kbs <kbs21c1987@gmail.com>
2021-12-02 14:54:19 +0900

    js device

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
         //alert(index);
 
         var section = cont.eq(index);
-        var offset = section.offset().top - 50;
+        var offset = section.offset().top;
         
         $("html, body").stop().animate({ scrollTop: offset },500);
         
@@ -27,7 +27,7 @@ $(document).ready(function(){
         //alert(index);
 
         var section = cont.eq(index);
-        var offset = section.offset().top - 50;
+        var offset = section.offset().top;
         
         $("html, body").stop().animate({ scrollTop: offset },500);
         
