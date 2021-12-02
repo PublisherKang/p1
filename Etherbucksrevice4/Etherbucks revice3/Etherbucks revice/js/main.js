5e0098ed13514ca80de6d3db5ba6412aac5dfc7f
vstro kbs <kbs21c1987@gmail.com>
2021-12-02 10:21:38 +0900

    etherbuck

---
+++ Etherbucksrevice4/Etherbucks revice3/Etherbucks revice/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    // pc 메뉴 이동
+    var nav = $(".gnb ul li");
+    var cont = $("#container > section");
+
+    var sub_nav = $(".gnb_mobile ul li")
+
+    nav.click(function(c){
+        c.preventDefault();
+        var target = $(this);
+        var index = target.index();
+        //alert(index);
+
+        var section = cont.eq(index);
+        var offset = section.offset().top - 50;
+        
+        $("html, body").stop().animate({ scrollTop: offset },500);
+        
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+    });
+
+    sub_nav.click(function(c){
+        c.preventDefault();
+        var target = $(this);
+        var index = target.index();
+        //alert(index);
+
+        var section = cont.eq(index);
+        var offset = section.offset().top - 50;
+        
+        $("html, body").stop().animate({ scrollTop: offset },500);
+        
+        $("#mobile_sideMenu").css("display", "none");
+    });
+
+
+
+    $(".mobi_button").click(function(){
+        $("#mobile_sideMenu").css("display", "block");
+        $(".gnb_mobile").css({
+            'transform': 'translateX(0)',
+        })
+    })
+
+    $(".closeBtn").click(function(){
+        $("#mobile_sideMenu").css("display", "none");
+    })
+})
